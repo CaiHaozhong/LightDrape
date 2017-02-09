b150e3eb29774d8c446108c86708e07e84b72622
CaiHaozhong <chz4298@gmail>
2017-02-09 11:35:52 +0800

    sync

--- LightDrape/LevelSetSkeletonFitter.cpp
+++ LightDrape/LevelSetSkeletonFitter.cpp
@@ -1,5 +1,6 @@
 #include "LevelSetSkeletonFitter.h"
-
+#include "LevelSet.h"
+#include <vector>
 
 LevelSetSkeletonFitter::LevelSetSkeletonFitter(void)
 {
@@ -15,6 +16,18 @@ LevelSetSkeletonFitter::~LevelSetSkeletonFitter(void)
 VertexAlter_ LevelSetSkeletonFitter::fit( Region_ humanRegion )
 {
 	mHumanRegion = humanRegion;
+	std::vector<LevelCircle_> humanCircles;
+	std::vector<LevelCircle_> garmetCircles;
+	std::vector<double> delta;
+	size_t garCircleSize = garmetCircles.size();
+	size_t humanCircleSize = humanCircles.size();
+	delta.reserve(garCircleSize);
+	for(size_t i = 0; i < garCircleSize; i++){
+		if(i >= humanCircleSize){
+			// 比躯干长的部分保持不动
+			break;
+		}
+	}
 }
 
 std::string LevelSetSkeletonFitter::getName()
